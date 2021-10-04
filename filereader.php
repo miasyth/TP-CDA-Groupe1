833f616e2d5dfa22a62337d8d845f68ed182ce4c
Lucas Merino <lucas.merino@outlook.fr>
2021-10-04 12:01:37 +0200

    Modification des CSV et du filereader

--- filereader.php
+++ filereader.php
@@ -1,2 +1,22 @@
-<?php
+<?PHP
 
+// Définir le chemin d'accès au fichier CSV
+$csv = readline("Veuillez indiquer quel fichier vous souhaitez afficher : agence.csv|client.csv|compte.csv ");
+$tab = [];
+
+function read($csv){
+    $file = fopen($csv, 'r');
+    while (!feof($file) ) {
+        $line[] = fgetcsv($file, 1024, ";");
+    }
+    fclose($file);
+    return $line;
+}
+
+$csv = read($csv);
+$tab [] = $csv;
+
+print_r($csv);
+
+
+?>
