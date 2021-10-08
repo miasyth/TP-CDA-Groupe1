7f96108a79c06ee87ffce5d342221eed7e7e287f
Lucas Merino <lucas.merino@outlook.fr>
2021-10-08 16:47:36 +0200

    update agence.js et agence.json

--- Image/agence.js
+++ Image/agence.js
@@ -1,3 +1,19 @@
-<?php
-    
-?>
+
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("agences.json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("/Users/lucas/Desktop/Devs/Git/TP-Gestion-de-Comptes-Bancaires/Imag/agences.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
