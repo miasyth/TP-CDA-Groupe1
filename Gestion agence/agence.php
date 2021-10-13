9f55958f4c1b4c8467a642349e4aa9e7dc1d0823
Lucas Merino <lucas.merino@outlook.fr>
2021-10-13 10:37:34 +0200

    test agence.php

--- Gestion agence/agence.php
+++ Gestion agence/agence.php
@@ -6,22 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire de création d'agence.</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="Image/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-<h4>
-    Bonjour, avez bous bien entré cette Adresse <?php echo $_POST['nom'] ?> pour cette agence ? <?php echo $_POST['Agence'] ?>
-    Voici le code postal indiqué, <?php echo $_POST['Code_Postal'] ?> ainsi que le Téléphone <?php echo $_POST['Telephone'] ?> et le Mail <?php echo $_POST['Mail'] ?>.
-</h4>
-
 <?php
+    $tab = [];
+
     $Ag = $_POST['Agence'];
+    $tab=$_POST;
+    
     $Ad = $_POST['Adresse'];
+    $tab=$_POST;
+    
     $Cp = $_POST['Code_Postal'];
-    $T = $_POST['Télephone'];
+    $tab=$_POST;
+    
+    $T = $_POST['Téléphone'];
+    $tab=$_POST;
+    
     $M = $_POST['Mail'];
+    $tab=$_POST;
+    
+    foreach($tab as $key => $val){
+        print_r("$val |");
+    };
+    
 ?>
 
 <!--Voir pour transférer les valeurs dans un tableau appelé das une fonction, puis les renvoyer dans sur la page html et afficher le tout dans une case.-->
