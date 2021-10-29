ebc5260d489851c8ab7ee0d0b2166eeb286437b3
MaximilienHeaps <maximilien.heaps@hotmail.fr>
2021-10-29 16:02:15 +0200

    test

--- client/client.php
+++ client/client.php
@@ -1,6 +1,26 @@
 <?php 
 require ('classclient.php') ;
 
+function colorLog($str, $type = 'i'){
+    switch ($type) {
+        case 'e': //error
+            echo "\033[31m$str \033[0m\n";
+        break;
+        case 's': //success
+            echo "\033[32m$str \033[0m\n";
+        break;
+        case 'w': //warning
+            echo "\033[33m$str \033[0m\n";
+        break;  
+        case 'i': //info
+            echo "\033[36m$str \033[0m\n";
+        break;      
+        default:
+        # code...
+        break;
+    }
+}
+
 echo json_encode($_POST);
 
 $tab=[];
